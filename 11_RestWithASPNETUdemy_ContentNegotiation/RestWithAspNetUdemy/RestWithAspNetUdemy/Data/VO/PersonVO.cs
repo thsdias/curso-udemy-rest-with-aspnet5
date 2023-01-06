7a1e7fe7f579dcd6c84757dc90955bc9e91a5e17
thsdias <thdias.84@gmail.com>
2023-01-05 21:48:49 -0300

    Content Negotiation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithAspNetUdemy/RestWithAspNetUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,13 @@
+﻿namespace RestWithAspNetUdemy.Model
+{
+    public class PersonVO
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+        
+        public string Gender { get; set; }
+    }
+}
---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithAspNetUdemy/RestWithAspNetUdemy/Startup.cs
@@ -0,0 +1,106 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Net.Http.Headers;
+using RestWithAspNetUdemy.Business;
+using RestWithAspNetUdemy.Business.Implementations;
+using RestWithAspNetUdemy.Model.Context;
+using RestWithAspNetUdemy.Repository;
+using RestWithAspNetUdemy.Repository.Generic;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithAspNetUdemy
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            if (Environment.IsDevelopment())
+            {
+                MigrationDataBase(connection);
+            }
+
+            // Adiciona formato XML.
+            services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+            })
+            .AddXmlSerializerFormatters();
+
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
+
+            // Versionamento API.
+            services.AddApiVersioning();
+
+            // Injecao de Dependencia.
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IBooksBusiness, BooksBusinessImplementation>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private void MigrationDataBase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
+        }
+    }
+}
