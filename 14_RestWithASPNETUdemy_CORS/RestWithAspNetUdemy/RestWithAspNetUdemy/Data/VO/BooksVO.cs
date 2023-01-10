e5d91cdb447c998cc26aa38271faff53e161054e
thsdias <thdias.84@gmail.com>
2023-01-09 21:48:46 -0300

    Configurando CORS

---
+++ 14_RestWithASPNETUdemy_CORS/RestWithAspNetUdemy/RestWithAspNetUdemy/Data/VO/BooksVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RestWithAspNetUdemy.Model
+{
+    public class BooksVO
+    {
+        public long Id { get; set; }
+
+        public String Author { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ 14_RestWithASPNETUdemy_CORS/RestWithAspNetUdemy/RestWithAspNetUdemy/Model/Person.cs
@@ -0,0 +1,21 @@
+﻿using RestWithAspNetUdemy.Business.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithAspNetUdemy.Model
+{
+    [Table("person")]
+    public class Person : BaseEntity
+    {
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName{ get; set; }
+
+        [Column("address")]
+        public string Adress { get; set; }
+        
+        [Column("gender")]
+        public string Gender { get; set; }
+    }
+}
