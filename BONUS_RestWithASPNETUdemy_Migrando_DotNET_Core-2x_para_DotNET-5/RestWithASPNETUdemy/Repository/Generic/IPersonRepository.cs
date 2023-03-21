85e7fceb6e27140d051e943ecb37237d5e1edd9b
thsdias <thdias.84@gmail.com>
2023-03-21 20:29:49 -0300

    Migrando do .NET Core 2x para .NET 5

---
+++ BONUS_RestWithASPNETUdemy_Migrando_DotNET_Core-2x_para_DotNET-5/RestWithASPNETUdemy/Repository/Generic/IPersonRepository.cs
@@ -0,0 +1,9 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Repository.Generic {
+    public interface IPersonRepository : IRepository<Person>
+    {
+        List<Person> FindByName(string fristName, string lastName);
+    }
+}
